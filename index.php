025bf85e300339785575d98bc7cfea92c28dd61c
3125969330 <106980624+3125969330@users.noreply.github.com>
2024-11-14 00:10:11 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,452 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!-- basic -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<!-- mobile metas -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="initial-scale=1, maximum-scale=1">
+<!-- site metas -->
+<title>Circuit-Braker</title>
+<meta name="keywords" content="">
+<meta name="description" content="">
+<meta name="author" content="">
+<!-- site icons -->
+<link rel="icon" href="images/fevicon/fevicon.png" type="image/gif" />
+<!-- bootstrap css -->
+<link rel="stylesheet" href="css/bootstrap.min.css" />
+<!-- Site css -->
+<link rel="stylesheet" href="css/style.css" />
+<!-- responsive css -->
+<link rel="stylesheet" href="css/responsive.css" />
+<!-- colors css -->
+<link rel="stylesheet" href="css/colors1_dark.css" />
+<!-- custom css -->
+<link rel="stylesheet" href="css/custom.css" />
+<!-- wow Animation css -->
+<link rel="stylesheet" href="css/animate.css" />
+<!-- revolution slider css -->
+<link rel="stylesheet" type="text/css" href="revolution/css/settings.css" />
+<link rel="stylesheet" type="text/css" href="revolution/css/layers.css" />
+<link rel="stylesheet" type="text/css" href="revolution/css/navigation.css" />
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+      <![endif]-->
+</head>
+<body id="default_theme" class="it_service">
+<!-- loader -->
+<div class="bg_load"> <img class="loader_animation" src="images/loaders/LOGO1.png" alt="#" /> </div>
+<!-- end loader -->
+<!-- header -->
+<header id="default_header" class="header_style_1">
+  <!-- header top -->
+  <div class="header_top">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <div class="full">
+            <div class="topbar-left">
+              <ul class="list-inline">
+                <li> <span class="topbar-label"><i class="fa  fa-home"></i></span> <span class="topbar-hightlight">Colegio la nueva estancia 2024</span> </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 right_section_header_top">
+          <div class="float-left">
+            <div class="social_icon">
+              <ul class="list-inline">
+                <li><a class="fa fa-facebook" href="https://www.facebook.com/" title="Facebook" target="_blank"></a></li>
+                <li><a class="fa fa-linkedin" href="https://www.linkedin.com" title="LinkedIn" target="_blank"></a></li>
+                <li><a class="fa fa-instagram" href="https://www.instagram.com/circuit_braker_02/" title="Instagram" target="_blank"></a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="float-right">
+            <div class="make_appo"> <a class="btn white_btn" href="make_appointment.html">Generar cita</a> </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end header top -->
+  <!-- header bottom -->
+  <div class="header_bottom">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-3 col-md-12 col-sm-12 col-xs-12">
+        <!-- Inicio del logo -->
+        <div class="logo"> 
+          <a href="INDEX.PHP">
+            <img src="images/logos/LOGO1.png" alt="logo" />
+          </a> 
+        </div>
+        <!-- Fin del logo -->
+      </div>
+      <div class="col-lg-9 col-md-12 col-sm-12 col-xs-12">
+        <!-- Inicio del menú -->
+        <div class="menu_side">
+          <div id="navbar_menu">
+            
+        <!-- Fin del menú -->
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Fin del encabezado inferior -->
+</header>
+<!-- Fin del encabezado -->
+<!-- Sección -->
+<div id="slider" class="section main_slider">
+  <div class="container-fuild">
+    <div class="row">
+      <div id="rev_slider_4_1_wrapper" class="rev_slider_wrapper fullwidthbanner-container" data-alias="classicslider1" style="margin:0px auto;background-color:transparent;padding:0px;margin-top:0px;margin-bottom:0px;">
+        <!-- INICIO DEL SLIDER REVOLUTION -->
+        <div id="rev_slider_4_1" class="rev_slider fullwidthabanner" style="display:none;" data-version="5.0.7">
+          <ul>
+            <li data-index="rs-1812" data-transition="zoomin" data-slotamount="7" data-easein="Power4.easeInOut" data-easeout="Power4.easeInOut" data-masterspeed="2000" data-thumb="images/it_service/slide1.jpg" data-rotate="0" data-saveperformance="off" data-title="Servicios Informáticos" data-description="">
+              <!-- IMAGEN PRINCIPAL -->
+              <img src="images/it_service/slide1.jpg" alt="#" data-bgposition="center center" data-kenburns="on" data-duration="30000" data-ease="Linear.easeNone" data-scalestart="100" data-scaleend="120" data-rotatestart="0" data-rotateend="0" data-offsetstart="0 0" data-offsetend="0 0" data-bgparallax="10" class="rev-slidebg" data-no-retina>
+              <!-- CAPAS Y TEXTOS -->
+              <div class="tp-caption NotGeneric-Title tp-resizeme rs-parallaxlevel-0" 
+                data-x="center" data-y="middle" data-voffset="0"
+                style="z-index: 6; white-space: nowrap;">Servicios Informáticos</div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Fin de la sección -->
+             <div class="tp-caption NotGeneric-SubTitle   tp-resizeme rs-parallaxlevel-0" 
+                              id="slide-18-layer-4" 
+                              data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" 
+                              data-y="['middle','middle','middle','middle']" data-voffset="['52','51','51','31']" 
+                              data-width="none"
+                              data-height="none"
+                              data-whitespace="nowrap"
+                              data-transform_idle="o:1;"
+                              data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;" 
+                              data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;" 
+                              data-mask_in="x:0px;y:[100%];s:inherit;e:inherit;" 
+                              data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;" 
+                              data-start="1500" 
+                              data-splitin="none" 
+                              data-splitout="none" 
+                              data-responsive_offset="on" 
+                         
+            </li>
+          </ul>
+          <div class="tp-static-layers"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Fin de sección -->
+<!-- Sección -->
+<div class="section padding_layout_1">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="full">
+          <div class="main_heading text_align_center">
+            <h2>¿Por qué elegirnos?</h2>
+            <p class="large">¡El servicio de reparación más rápido con el mejor precio!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+        <div class="full text_align_center margin_bottom_30">
+          <div class="center">
+            <div class="icon"> <img src="images/it_service/i1.png" alt="#" /> </div>
+          </div>
+          <h4 class="theme_color">Recuperación de datos</h4>
+         
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+        <div class="full text_align_center margin_bottom_30">
+          <div class="center">
+            <div class="icon"> <img src="images/it_service/i2.png" alt="#" /> </div>
+          </div>
+          <h4 class="theme_color">Reparación de computadoras</h4>
+          
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+        <div class="full text_align_center margin_bottom_30">
+          <div class="center">
+            <div class="icon"> <img src="images/it_service/i3.png" alt="#" /> </div>
+          </div>
+          <h4 class="theme_color">Servicio móvil</h4>
+          
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+        <div class="full text_align_center margin_bottom_30 margin_0">
+          <div class="center">
+            <div class="icon"> <img src="images/it_service/i4.png" alt="#" /> </div>
+          </div>
+          <h4 class="theme_color">Soluciones de red</h4>
+          
+        </div>
+      </div>
+    </div>
+    <!-- Más contenido traducido -->
+  </div>
+</div>
+
+<div class="section padding_layout_1 light_silver gross_layout right_gross_layout">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="full">
+          <div class="main_heading text_align_right">
+            <h2>Nuestra opinión</h2>
+            <p class="large">Una forma fácil y efectiva de reparar su dispositivo</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row counter">
+      <div class="col-md-4"> </div>
+      <div class="col-md-8">
+        <div class="row">
+          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 margin_bottom_50">
+            <div class="text_align_right"><i class="fa fa-smile-o"></i></div>
+            <div class="text_align_right">
+              <p class="counter-heading text_align_right">
+Clientes satisfechos</p>
+            </div>
+            <h5 class="counter-count">2150</h5>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 margin_bottom_50">
+            <div class="text_align_right"><i class="fa fa-laptop"></i></div>
+            <div class="text_align_right">
+              <p class="counter-heading text_align_right">Computadoras portátil reparadas</p>
+            </div>
+            <h5 class="counter-count">1280</h5>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 margin_bottom_50">
+            <div class="text_align_right"><i class="fa fa-desktop"></i></div>
+            <div class="text_align_right">
+              <p class="counter-heading">CComputadora reparada</p>
+            </div>
+            <h5 class="counter-count">848</h5>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 margin_bottom_50">
+            <div class="text_align_right"><i class="fa fa-windows"></i></div>
+            <div class="text_align_right">
+              <p class="counter-heading">
+Sistema operativo instalado</p>
+            </div>
+            <h5 class="counter-count">450</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- end section -->
+<!-- section -->
+<div class="section padding_layout_1">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="full">
+          <div class="main_heading text_align_left">
+            <h2>Lo ultimo en nuestras reparaciónes</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4">
+        <div class="full blog_colum">
+          <div class="blog_feature_img"> <img src="imagen.jpeg" alt="#" /> </div>
+          <div class="blog_feature_head">
+            <h4>Reparacion de teclados del colegio la nueva estancia</h4>
+          </div>
+          <div class="blog_feature_cont">
+            <p>Generamos nuestros primeros pininos con la reparación de equipos de computos como lo son los teclados de nuestros colegios los cuales 
+            generamos una previa reparación frente a estos equipos que se encontraban con falencias tecnicas </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="full blog_colum">
+          <div class="blog_feature_img"> <img src="imagen1.jpeg" alt="#" /> </div>
+          <div class="post_time">
+          </div>
+          <div class="blog_feature_head">
+            <h4>Resultados obtenidos</h4>
+          </div>
+          <div class="blog_feature_cont">
+              <p>Como estudiantes, logramos devolver a funcionamiento varios teclados que presentaban fallas técnicas. Este logro no solo permitió optimizar los recursos tecnológicos de nuestro colegio, sino que también fortaleció nuestras habilidades técnicas y el trabajo colaborativo</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="full blog_colum">
+          <div class="blog_feature_img"> <img src="imagen2.jpeg" alt="#" /> </div>
+          
+          <div class="blog_feature_head">
+            <h4>Innovación desde el aula</h4>
+          </div>
+          <div class="blog_feature_cont">
+            <p>En este proyecto, no solo solucionamos problemas técnicos, sino que también propusimos nuevas ideas para mantener los equipos en buen estado. Diseñamos estrategias preventivas que buscan garantizar la durabilidad de los recursos tecnológicos, mostrando que nuestra creatividad e iniciativa pueden generar impacto positivo.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- footer -->
+<footer class="footer_style_2">
+  
+      <div class="cprt">
+        <p> Circuit Braker 2024© </p>
+      </div>
+    </div>
+  </div>
+</footer>
+<!-- end footer -->
+<!-- js section -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- menu js -->
+<script src="js/menumaker.js"></script>
+<!-- wow animation -->
+<script src="js/wow.js"></script>
+<!-- custom js -->
+<script src="js/custom.js"></script>
+<!-- revolution js files -->
+<script src="revolution/js/jquery.themepunch.tools.min.js"></script>
+<script src="revolution/js/jquery.themepunch.revolution.min.js"></script>
+<script src="revolution/js/extensions/revolution.extension.actions.min.js"></script>
+<script src="revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+<script src="revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+<script src="revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script src="revolution/js/extensions/revolution.extension.migration.min.js"></script>
+<script src="revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+<script src="revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+<script src="revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script src="revolution/js/extensions/revolution.extension.video.min.js"></script>
+<!-- map js -->
+<script>
+         // This example adds a marker to indicate the position of Bondi Beach in Sydney,
+         // Australia.
+         function initMap() {
+           var map = new google.maps.Map(document.getElementById('map'), {
+             zoom: 11,
+             center: {lat: 40.645037, lng: -73.880224},
+         styles: [
+                  {
+                    elementType: 'geometry',
+                    stylers: [{color: '#fefefe'}]
+                  },
+                  {
+                    elementType: 'labels.icon',
+                    stylers: [{visibility: 'off'}]
+                  },
+                  {
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#616161'}]
+                  },
+                  {
+                    elementType: 'labels.text.stroke',
+                    stylers: [{color: '#f5f5f5'}]
+                  },
+                  {
+                    featureType: 'administrative.land_parcel',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#bdbdbd'}]
+                  },
+                  {
+                    featureType: 'poi',
+                    elementType: 'geometry',
+                    stylers: [{color: '#eeeeee'}]
+                  },
+                  {
+                    featureType: 'poi',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#757575'}]
+                  },
+                  {
+                    featureType: 'poi.park',
+                    elementType: 'geometry',
+                    stylers: [{color: '#e5e5e5'}]
+                  },
+                  {
+                    featureType: 'poi.park',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#9e9e9e'}]
+                  },
+                  {
+                    featureType: 'road',
+                    elementType: 'geometry',
+                    stylers: [{color: '#eee'}]
+                  },
+                  {
+                    featureType: 'road.arterial',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#3d3523'}]
+                  },
+                  {
+                    featureType: 'road.highway',
+                    elementType: 'geometry',
+                    stylers: [{color: '#eee'}]
+                  },
+                  {
+                    featureType: 'road.highway',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#616161'}]
+                  },
+                  {
+                    featureType: 'road.local',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#9e9e9e'}]
+                  },
+                  {
+                    featureType: 'transit.line',
+                    elementType: 'geometry',
+                    stylers: [{color: '#e5e5e5'}]
+                  },
+                  {
+                    featureType: 'transit.station',
+                    elementType: 'geometry',
+                    stylers: [{color: '#000'}]
+                  },
+                  {
+                    featureType: 'water',
+                    elementType: 'geometry',
+                    stylers: [{color: '#c8d7d4'}]
+                  },
+                  {
+                    featureType: 'water',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#b1a481'}]
+                  }
+                ]
+         });
+         
+           var image = 'images/it_service/location_icon_map_cont.png';
+           var beachMarker = new google.maps.Marker({
+             position: {lat: 40.645037, lng: -73.880224},
+             map: map,
+             icon: image
+           });
+         }
+      </script>
+<!-- google map js -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA8eaHt9Dh5H57Zh0xVTqxVdBFCvFMqFjQ&callback=initMap"></script>
+<!-- end google map js -->
+</body>
+</html>
